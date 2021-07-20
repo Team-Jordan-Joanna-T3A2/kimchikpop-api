237071870a22888cb5a25239ddf3b2087f43fe16
Joanna-S-Lee <annaoj91@gmail.com>
2021-07-20 13:24:07 +1000

    defined routes and define scope of API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    get '/users', to: 'users#index'
+    post '/users', to: 'users#create'
+    get '/users/:id', to: 'users#show'
+    put '/users/:id', to: 'users#update'
+    delete '/users/:id', to: 'users#destroy'
+    
+    get '/reservations', to: 'reservations#index'
+    post '/reservations', to: 'reservations#create'
+    get '/reservations/:id', to: 'reservations#show'
+    put '/reservations/:id', to: 'reservations#update'
+    delete '/reservations/:id', to: 'reserations#destroy'
+  end
 end
