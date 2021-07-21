726bf21f42dcfee8dda6c573f001b800118a60ee
Joanna-S-Lee <annaoj91@gmail.com>
2021-07-21 15:09:29 +1000

    tested requests with postman

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,15 +1,22 @@
 class ReservationsController < ApplicationController
     def index
-        @reservations = Reservation.all
-        render json: @reservations
+        render json: Reservation.all
     end
 
-    # def create
-    #     @reservation = Reservation.create(reservation_params)
-    # end
+    def create
+        @reservation = Reservation.new(reservation_params)
+        if @reservation.save
+            #if the reservation saves successfully
+            render json: @reservation, status: :created
+        else
+            #if it errors out
+            render json: @reservation.errors, status: :unprocessable_entity    
+        end
+    end
 
-    # private
-    # def reservation_params
-    #     params.permit()
-    # end
+    private
+
+    def reservation_params
+        params.require(:reservation).permit(:first_name, :last_name, :email, :phone_number, :time, :pax, :code, :confirmed)
+    end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,3 @@
 class Reservation < ApplicationRecord
+    validates :first_name, :last_name, :email, :phone_number, :time, :pax, :code, :confirmed, presence: true
 end
