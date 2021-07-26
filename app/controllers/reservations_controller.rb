052a09f147c822218c92816a25feefd7f0ef2c15
Joanna-S-Lee <annaoj91@gmail.com>
2021-07-26 20:05:45 +1000

    changed code generation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,7 @@
 class ReservationsController < ApplicationController
   before_action :authenticated!, only: %i[index update destroy]
   before_action :set_reservation, only: %i[update destroy show]
+  
 
   def index
     render json: Reservation.all.order(id: :asc)
@@ -12,9 +13,11 @@ class ReservationsController < ApplicationController
 
   def create
     @reservation = Reservation.new(reservation_params)
+    @reservation.code = generate
     if @reservation.save
       #if the reservation saves successfully
-      render json: @reservation, status: :created, code: ReservationHelper.generate
+      render json: @reservation, status: :created 
+      # code: ReservationHelper.generate
     else
       #if it errors out
       render json: @reservation.errors, status: :unprocessable_entity
@@ -42,5 +45,16 @@ class ReservationsController < ApplicationController
 
   def reservation_params
     params.require(:reservation).permit(:first_name, :last_name, :email, :phone_number, :time, :pax, :code, :confirmed)
+  end
+
+  def generate
+    new_code = SecureRandomString.new(6, lowercase: :false)
+    uniq = true
+    Reservation.all.each { |reservation| uniq = false if reservation.code = new_code }
+    if uniq = true
+      return new_code
+    else
+      generate
+    end
   end
 end
