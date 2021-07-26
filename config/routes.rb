3f086533c8f6083ccbc9532c91e601772bbc9909
arbpotatoes <arbpotatoes@gmail.com>
2021-07-26 22:08:51 +1000

    Connected multiple features to API. Worked on customer portal

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope "/api" do
     get "/users", to: "users#index"
-    post "/users", to: "users#create"
+    post "/sign-up", to: "users#create"
     get "/users/:id", to: "users#show"
     put "/users/:id", to: "users#update"
     delete "/users/:id", to: "users#destroy"
+    post "users/login", to: "users#"
 
     get "/reservations", to: "reservations#index"
     post "/reservations", to: "reservations#create"
