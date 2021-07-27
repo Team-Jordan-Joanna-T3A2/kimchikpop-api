74e6d704ff0ab2098f150aa1022c492a522da859
arbpotatoes <arbpotatoes@gmail.com>
2021-07-27 12:58:12 +1000

    Couple changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,38 +1,37 @@
 class ApplicationController < ActionController::API
 
-    # def decode()
-        
-    # end
+  # def decode()
 
-    def encode(payload)
-        JWT.encode(payload, Rails.application.credentials.jwt_secret_key,'HS512')
+  # end
+
+  def encode(payload)
+    JWT.encode(payload, Rails.application.credentials.jwt_secret_key, "HS512")
+  end
+
+  def authenticated!
+    begin
+      token = request.authorization.split(" ")[1]
+      decoded = JWT.decode(token, Rails.application.credentials.jwt_secret_key, true, algorithm: "HS512")
+      user_id = decoded[0]["user_id"]
+      @user = User.find(user_id)
+    rescue JWT::DecodeError
+      render json: { message: "Unauthorized: Please log in" }
     end
+  end
 
+  protected
 
-    def authenticated!
-        begin
-          token = request.authorization.split(' ')[1]
-          decoded = JWT.decode(token, Rails.application.credentials.jwt_secret_key, true, algorithm: 'HS512')
-          user_id = decoded[0]['user_id']
-          @user = User.find(user_id)
-        rescue JWT::DecodeError
-          render json: { message: 'Unauthorized: Please log in' }
-        end
+  def verify_admin!
+    unless @user.admin?
+      render json: { message: "You do not have admin privileges" } and return
     end
-    
-    protected
+  end
 
-    def verify_admin!
-      unless @user.admin?
-        render json: { message: 'You do not have admin privileges' } and return
-      end
-    end
-
-    # def authenticated
-    #    token = request.authorization().split(' ')[1]
-    #    decoded = JWT.decode(token, Rails.application.credentials.jwt_secret_key, true, algorithm: 'HS512')
-    #    user_id = decoded[0]['user_id']
-    #    @user = User.find(user_id)
-    #    render json: { message: 'Unauthorized: Please log in'} unless @user             
-    # end
+  # def authenticated
+  #    token = request.authorization().split(' ')[1]
+  #    decoded = JWT.decode(token, Rails.application.credentials.jwt_secret_key, true, algorithm: 'HS512')
+  #    user_id = decoded[0]['user_id']
+  #    @user = User.find(user_id)
+  #    render json: { message: 'Unauthorized: Please log in'} unless @user
+  # end
 end
