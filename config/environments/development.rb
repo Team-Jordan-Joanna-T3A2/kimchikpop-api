9b5bc22ebf3fa059a01480f8cbc8d5b766bcca39
Joanna-S-Lee <annaoj91@gmail.com>
2021-07-27 12:52:19 +1000

    Mailgun added, with domain credentials.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :local
+
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain => Rails.application.credentials.mailgun[:domain],
+    :user_name => Rails.application.credentials.mailgun[:user_name],
+    :password => Rails.application.credentials.mailgun[:password]
+}
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
